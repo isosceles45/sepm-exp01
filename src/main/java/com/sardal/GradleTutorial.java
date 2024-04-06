d25f925c95d86b1478ebf2ec9d5bb3b628be7a96
isosceles45 <atharvasardal06@gmail.com>
2024-04-06 16:55:37 +0530

    gradle built

---
+++ src/main/java/com/sardal/GradleTutorial.java
@@ -0,0 +1,7 @@
+package com.sardal;
+
+public class GradleTutorial {
+	public static void main(String[] args) {
+		System.out.println("SEPM EXP 01");
+	}
+}
---
+++ src/test/java/com/sardal/GradleTutorialTest.java
@@ -0,0 +1,10 @@
+package com.sardal;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[] {});
+    }
+}
